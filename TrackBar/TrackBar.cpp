f10525229903a3d1458cb23ba3da539cfb653797
jiawentao <wentaojia2014@163.com>
2017-11-09 00:18:17 +0800

    TrackBar 调节亮度和对比度

--- TrackBar/TrackBar.cpp
+++ TrackBar/TrackBar.cpp
@@ -3,28 +3,40 @@
 using namespace std;
 using namespace cv;
 
-int gBrigheness = 160;
+int gBrigheness = 80;
+int gContrast = 80;
+Mat image, dst;
 const string windowName = "Window";
 const string brightnessBarName = "brightnessBar";
 const string contrastBarName = "contrastBar";
-Mat image;
-void onChange(int value, void *data) {
-	Mat bw = gBrigheness < 128 ? (image < gBrigheness) : (image > gBrigheness);
-	vector<vector<Point>> contours;
-	vector<Vec4i> hierarchy;
-	findContours(bw, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
 
-	Mat dst = Mat::zeros(image.size(), CV_8UC1);
-	if (!contours.empty() && !hierarchy.empty()) {
-		int index = 0;
-		while (index >= 0) {
-			Scalar color((rand() & 255), (rand() & 255), (rand() & 255));
-			drawContours(dst, contours, index, color, CV_FILLED, 8, hierarchy);
-			index = hierarchy[index][0];
+void onChange(int value, void *data) {
+	for (int i = 0; i < image.rows; ++i) {
+		for (int j = 0; j < image.cols; ++j) {
+			dst.at<Vec3b>(i, j)[0] = saturate_cast<uchar>(gContrast * 0.01 * (image.at<Vec3b>(i, j)[0]) + gBrigheness);
+			dst.at<Vec3b>(i, j)[1] = saturate_cast<uchar>(gContrast * 0.01 * (image.at<Vec3b>(i, j)[1]) + gBrigheness);
+			dst.at<Vec3b>(i, j)[2] = saturate_cast<uchar>(gContrast * 0.01 * (image.at<Vec3b>(i, j)[2]) + gBrigheness);
 		}
 	}
 	imshow(windowName, dst);
 }
+//void onChange(int value, void *data) {
+//	Mat bw = gBrigheness < 128 ? (image < gBrigheness) : (image > gBrigheness);
+//	vector<vector<Point>> contours;
+//	vector<Vec4i> hierarchy;
+//	findContours(bw, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+//
+//	Mat dst = Mat::zeros(image.size(), CV_8UC1);
+//	if (!contours.empty() && !hierarchy.empty()) {
+//		int index = 0;
+//		while (index >= 0) {
+//			Scalar color((rand() & 255), (rand() & 255), (rand() & 255));
+//			drawContours(dst, contours, index, color, CV_FILLED, 8, hierarchy);
+//			index = hierarchy[index][0];
+//		}
+//	}
+//	imshow(windowName, dst);
+//}
 int main(int argc, char ** argv)
 {
 	if (argc != 2) {
@@ -36,10 +48,12 @@ int main(int argc, char ** argv)
 		cout << "read failed " << endl;
 		return -1;
 	}
-	
+	dst = Mat::zeros(image.size(), image.type());
 	namedWindow(windowName);
-	createTrackbar(brightnessBarName, windowName, &gBrigheness, 255, onChange);
+	createTrackbar(brightnessBarName, windowName, &gBrigheness, 300, onChange);
+	createTrackbar(contrastBarName, windowName, &gContrast, 300, onChange);
 	onChange(gBrigheness, 0);
+	onChange(gContrast, 0);
 	waitKey(0);
 	return 0;
 }
